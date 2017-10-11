4796cb2a80c694db78d3ff3a0383258e4f6d9dcc
Joyce Lam <joyce@joystudio.ca>
2017-10-11 10:24:29 -0700

    added my phonegap project

---
+++ taptaptap/www/js/main.js
@@ -0,0 +1,40 @@
+
+$(function(){
+	
+	/*$("div.touch").on("tap",function(){
+	  alert("you tapped me");
+	});
+	
+	$("div.touch").on("taphold",function(){
+	  alert("you tapped and held me");
+	});
+	*/
+	
+	$("div.touch").on("swipeleft",function(){
+	  //alert("no");
+	  
+	  //if you swipeLEFT, add class no = TURNS RED
+	  //if has class yes, remove yes
+	  if( $(this).hasClass("yes") ){
+		  $(this).removeClass("yes");
+	  }
+
+	  //add class no
+	  $(this).addClass("no");
+	  console.log("swiped left");
+	});
+	
+	$("div.touch").on("swiperight",function(){
+	  //alert("yes");
+	  //if swipeRIGHT, add class yes = TURNS GREEN
+	  //if has class no, remove no
+	  if( $(this).hasClass("no") ){
+		  $(this).removeClass("no");
+	  }
+	  //add class yes	
+	 	$(this).addClass("yes");
+	 	console.log("swiped right")
+	});
+
+		
+});
